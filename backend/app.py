5adf5c0bcab65101f44017068a6766d5c2de7b2e
Miłosz Gołębiewski <develimgo@gmail.com>
2024-07-02 22:35:08 +0200

    adding CORS to flask app

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
+import json
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173","http://localhost:5173"])
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+@app.route('/program')
+def getProgramData():
+    with open('program.json') as f:
+        response = json.load(f)
+        return response
